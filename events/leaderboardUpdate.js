a52c49cc8b65b46c64f7244e699367ce6c232583
Dylan van Bunnik <bungohdev@gmail.com>
2023-12-18 14:17:01 -0500

    add losses.

--- events/leaderboardUpdate.js
+++ events/leaderboardUpdate.js
@@ -21,11 +21,13 @@ async function updateLeaderboard(client) {
         const rank = index + 1;
         const playerName = player.username.split('#')[0];
         const wins = player.stats.wins;
+        const losses = player.stats.losses;
         const kills = player.stats.kills;
         const deaths = player.stats.deaths;
         const assists = player.stats.assists;
         // return `${rank}) \`${playerName}\` >> **${wins}** Wins **${kills}** Kills **${deaths}** Deaths **${assists}** Assists`;
-        return `**${rank}) ${playerName}** - ${wins} Wins :trophy:  ${kills} Kills :dagger: ${deaths} Deaths :skull:  ${assists} Assists :crossed_swords:`;
+        // return `**${rank}) ${playerName}**: ${wins} Wins :trophy: ${losses} Losses :flag_white: ${kills} Kills :dagger: ${deaths} Deaths :skull:  ${assists} Assists :crossed_swords:`;
+        return `**${rank}) ${playerName}**: \`${wins}W/${losses}L\` ${kills} Kills :dagger: ${deaths} Deaths :skull:  ${assists} Assists :crossed_swords:`;
     }).join('\n');
 
     const embed = {
